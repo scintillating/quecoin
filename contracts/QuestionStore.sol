cea6381176069bc4814a92d28b31863e78ad05ec
Kevin Liu <kevin@potatofrom.space>
2018-02-18 10:25:19 -0500

    Fix merge conflict

--- contracts/QuestionStore.sol
+++ contracts/QuestionStore.sol
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-pragma solidity ^0.4.18;
-=======
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
@@ -9,4 +6,3 @@ import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
 contract QuestionStore is Pausable {
 
 }
->>>>>>> 0c9a33e9c81183ecaa073c35aae1069321054775
