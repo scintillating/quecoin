33a1b2831e44043c2422bbfeeb39830035684d02
Ajacuzzi <34138509+Ajacuzzi@users.noreply.github.com>
2018-02-19 17:27:35 -0500

    Update Home.js

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
           <header className="jumbotron fadeInDown animated page-header" id="banner">
             <img src="https://i.imgur.com/vJwufW6.png" id="QueCoinLogo" className="fadeInDown animatedDelay"/>
             <p className="fadeInLeft animatedDelay text-warning">
-              <em>Ask and answer intelligent questions with ease, while making bank.</em>
+              <em>Sourcing and answering high-quality questions</em>
             </p> <br></br>
             <p>QueCoin incentivizes both the answerer, asker and the supporters when asking intelligent questions, using uncensorable and decentralized data storage, so no entity can interfere.</p> <br></br>
             <p>For just a few QUE, you can submit questions to be answered, but keep in mind, these questions cost money and you get a return on your investment if the community believes that the question is worthwhile.</p>
