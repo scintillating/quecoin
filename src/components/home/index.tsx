fdc3f71f8b1fc3a33414662d3beb21e7b93d9c62
RektInPieces <kunalsharda@live.com>
2018-02-23 03:13:02 -0500

    shitty website initiated

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,66 @@
 import React, { Component } from "react";
 import QuecoinLogo from "../../images/quecoin-logo.png";
 import "../../animate.css";
-
+import "../../css/navbar.css"
 class Home extends Component {
   render() {
     return (
-      <div className="jumbotron bg-dark fadeInDown animated page-header">
+              <div className="navigation" id="nav">
+                  <div className="item user">
+                      <img src="images/bg_user.png" alt="" width="199" height="199" className="circle"/>
+                      <a href="#" className="icon" />
+                      <h2>User</h2>
+                      <ul>
+                          <li><a href="#">Profile</a></li>
+                          <li><a href="#">Properties</a></li>
+                          <li><a href="#">Privacy</a></li>
+                      </ul>
+                  </div>
+                  <div className="item home">
+                      <img src="images/bg_home.png" alt="" width="199" height="199" className="circle"/>
+                      <a href="#" className="icon" />
+                      <h2>Home</h2>
+                      <ul>
+                          <li><a href="#">Portfolio</a></li>
+                          <li><a href="#">Services</a></li>
+                          <li><a href="#">Contact</a></li>
+                      </ul>
+                  </div>
+                  <div className="item shop">
+                      <img src="images/bg_shop.png" alt="" width="199" height="199" className="circle"/>
+                      <a href="#" className="icon" />
+                      <h2>Shop</h2>
+                      <ul>
+                          <li><a href="#">Catalogue</a></li>
+                          <li><a href="#">Orders</a></li>
+                          <li><a href="#">Offers</a></li>
+                      </ul>
+                  </div>
+                  <div className="item camera">
+                      <img src="images/bg_camera.png" alt="" width="199" height="199" className="circle"/>
+                      <a href="#" className="icon" />
+                      <h2>Photos</h2>
+                      <ul>
+                          <li><a href="#">Gallery</a></li>
+                          <li><a href="#">Prints</a></li>
+                          <li><a href="#">Submit</a></li>
+                      </ul>
+                  </div>
+                  <div className="item fav">
+                      <img src="images/bg_fav.png" alt="" width="199" height="199" className="circle"/>
+                      <a href="#" className="icon" />
+                      <h2>Love</h2>
+                      <ul>
+                          <li><a href="#">Social</a></li>
+                          <li><a href="#">Support</a></li>
+                          <li><a href="#">Comments</a></li>
+                      </ul>
+                  </div>
+              </div>
+
+
+
+      /*<div className="jumbotron bg-dark fadeInDown animated page-header">
         <h1>
           <img
             src={QuecoinLogo}
@@ -31,7 +86,7 @@ class Home extends Component {
           community as you must stake QUE in order to earn QUE, creating a
           self-secure system handled by intelligent smart-contracts.
         </p>
-      </div>
+      </div>*/
     );
   }
 }
