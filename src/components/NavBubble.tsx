e44ca1a318ddfdcba641c4bfc82ec748452b647c
Kevin Liu <kevin@potatofrom.space>
2018-02-22 19:15:23 -0500

    Move NavBubble styles into separate file

--- src/components/NavBubble.tsx
+++ src/components/NavBubble.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import FontAwesome from "react-fontawesome";
 
+import "./NavBubble.css";
 import "font-awesome/css/font-awesome.min.css";
 import $ from "jquery";
 
